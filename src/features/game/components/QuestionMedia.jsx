021f4917f8cdbede94e4482e55c0132521b50b1a
Nicole Vizuete Personal <vizuetenicol@gmail.com>
2026-06-27 14:56:59 -0500

    fix: add referrerPolicy no-referrer to bypass CDN hotlink protection on question images

--- src/features/game/components/QuestionMedia.jsx
+++ src/features/game/components/QuestionMedia.jsx
@@ -4,6 +4,7 @@ export default function QuestionMedia({ url }) {
     <img
       src={url}
       alt="Imagen de la pregunta"
+      referrerPolicy="no-referrer"
       style={{ maxWidth: '100%', maxHeight: 220, objectFit: 'contain', borderRadius: 8, marginBottom: 12 }}
     />
   )
